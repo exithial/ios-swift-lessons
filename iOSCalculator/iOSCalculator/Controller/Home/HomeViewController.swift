91b97981fe2f01e504d1f68fd81e873cdaea4974
Enrique Solís Gálvez <esolis.externo@coopeuch.cl>
2021-11-28 18:46:07 -0300

    Project iOS Calculator - Initialize Project

---
+++ iOSCalculator/iOSCalculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  iOSCalculator
+//
+//  Created by Enrique Solís on 28-11-21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
