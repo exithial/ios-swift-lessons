7256cc6104e4ae5b7503be8b9a3d179deed90401
Enrique Solís Gálvez <esolis.externo@coopeuch.cl>
2021-12-07 19:09:57 -0300

    SwiftUI - Stacks (VStack, HStack y ZStack)

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Enrique Solís on 07-12-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
